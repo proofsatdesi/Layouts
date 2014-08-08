f0da8056b61a8a94cc94e661182b791539945775
proofsatdesi <proofs@desi.com>
2014-08-07 17:01:29 -0700

    Added Initial File Collection

---
+++ Copy Layouts to Local Directories.bat
@@ -0,0 +1,2 @@
+@copy "j:\corel draw folders\layouts\*.cly" "c:\program files\corel\graphics10\custom\layouts"
+@copy "j:\corel draw folders\layouts\*.cly" "c:\program files\corel\corel graphics 11\custom data\layouts"
